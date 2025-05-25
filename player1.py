a6e1173e28e35684524313c7e1425c3702bb9450
Dearlimg <1492568061@qq.com>
2025-05-25 22:53:31 +0800

    添加最终平台程序

--- player1.py
+++ player1.py
@@ -1,18 +1,14 @@
-
-from client import client
 from client.client import QuoridorClient
 
 
 def main():
-    # 创建客户端实例
     client = QuoridorClient(
-        server_url="http://192.168.1.102:5071",  # 根据实际情况修改服务器地址
+        server_url="http://192.168.1.102:5071",
         # server_url="http://127.0.0.1:5100",
-        name="AI_Player2",
-        password="password",
+        name="Joker",
+        password="password"
     )
 
-    # 运行客户端
     client.run()
 
 if __name__ == "__main__":
