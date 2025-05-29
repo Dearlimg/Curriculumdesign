dfabb4c4f69cb62b69f53cd332bc2ce06e7d63b2
Dearlimg <1492568061@qq.com>
2025-05-29 11:09:39 +0800

    最终提交

--- player1.py
+++ player1.py
@@ -3,8 +3,8 @@ from client.client import QuoridorClient
 
 def main():
     client = QuoridorClient(
-        # server_url="http://127.0.0.1:5071",
-        server_url="http://127.0.0.1:5100",
+        server_url="http://127.0.0.1:5071",
+        # server_url="http://127.0.0.1:5100",
         name="Joker",
         password="123"
     )
--- player2.py
+++ player2.py
@@ -3,7 +3,7 @@ from client.client import QuoridorClient
 
 def main():
     client = QuoridorClient(
-        server_url="http://192.168.83.201:5071",
+        server_url="http://192.168.5.16:5071/",
         # server_url="http://127.0.0.1:5100",
         name="BatMan",
         password="123"
